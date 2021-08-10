4f052ea42f74cec44d834f34c2d340ab5e57eb90
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-08-10 20:38:55 +0500

    Created all components and imported and used here.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useEffect } from 'react';
+import './App.css';
+import Balance from "./components/Balance"
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionForm from './components/TransactionForm';
+import TransactionHistory from "./components/TransactionHistory";
+import { GlobalProvider } from './context/GlobalState';
+import { configureNotifications } from './services/firebase';
 
 const App = () => {
+
+  useEffect(() => {
+    configureNotifications();
+  }, [])
+
   return (
-    <div>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionHistory />
+        <TransactionForm />
+      </div>
+    </GlobalProvider>
   );
 }
 
